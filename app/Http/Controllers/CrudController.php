29ac07cef4aee0441694bf35f757c7684bdbf98d
Razaul Munir <razaulmunir@gmail.com>
2020-08-18 13:00:47 +0600

    validator added at inputed data

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CrudController extends Controller
 {
@@ -19,6 +20,18 @@ class CrudController extends Controller
     }
 
     function saveUser(Request $request){
-        dd($request->all());
+        // dd($request->all());
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|max:20',
+            'age' => 'required',
+            'address' => 'required'
+        ]);
+
+        if($validator->passes()){
+            echo "success";
+        }
+        else{
+            echo "fail";
+        }
     }
 }
